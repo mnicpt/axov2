2b81757c0272af4a1ceaca4945bd7cf752240bcf
Steve Mask <smask@paypal.com>
2022-10-17 09:37:39 -0400

    remove node_modules

--- node_modules/post-robot/dist/module/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-exports.__esModule = true;
-exports.TYPES = void 0;
-// export something to force webpack to see this as an ES module
-const TYPES = true; // eslint-disable-next-line flowtype/require-exact-type
-
-exports.TYPES = TYPES;
--- node_modules/universal-serialize/src/index.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-
-export * from './serialize';
-export * from './deserialize';
-export * from './serializers';
-export * from './constants';
-export * from './common';
-export * from './types';
